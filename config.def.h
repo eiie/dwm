041141904fac62bcd75f799820d6714c4de31a68
eiie <eiie>
2024-05-17 09:53:59 +0200

    patch : movestack

--- config.def.h
+++ config.def.h
@@ -69,6 +69,8 @@ static const char *passmenucmd[]  = { "passmenu", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 static const char *screenshotcmd[]  = { "flameshot", "gui", NULL };
 
+#include "movestack.c"
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -84,6 +86,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
